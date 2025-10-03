c8949869a662cb8c09902dcb94c18ff90776de45
Lina Alejandra Fierro porras <52893751+Linafierro1@users.noreply.github.com>
2025-10-03 10:20:48 +0000

    Activación de Entorno

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/workspaces/SSPD/.venv"
+set -gx VIRTUAL_ENV "/workspaces/SSPD_Fierro_Porras/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
